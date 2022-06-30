ad6a6787dd3225aa18691f57afa67ba194f89454
durjoy sarkar <durjoywebdeveloper777@gmail.com>
2022-06-30 14:38:55 +0600

    p cheking

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ async function run(){
           const result = await userCollection.deleteOne(filter)
           res.send(result)
         })
-        app.delete('/order/:id',verifyJWT,async(req,res)=>{
+        app.delete('/order/:id',async(req,res)=>{
           const id = req.params.id;
           const filter = {_id:ObjectId(id)}
           const result = await ordersCollection.deleteOne(filter)
